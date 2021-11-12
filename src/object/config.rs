7b592a6d1bd26c02de859fbce1c81ef3de41e9af
Furkan Mustafa <furkan@s2n.io>
2021-11-12 23:38:52 +0900

    object::config: fix error return

--- src/object/config.rs
+++ src/object/config.rs
@@ -1,4 +1,4 @@
-use std::io::Error;
+use std::io::{Error,ErrorKind};
 use url::{Url, ParseError};
 
 use crate::object::ObjectStorage;
@@ -8,14 +8,14 @@ pub fn storage_with_config(config: String) -> Result<Box<dyn ObjectStorage>, Err
     let issue_list_url = Url::parse(&config)
         .expect("Failed to parse config (URL)");
 
-    match issue_list_url.scheme() {
+    return match issue_list_url.scheme() {
         "file" => {
             // Expecting a folder path
             Ok(Box::new(FileBackend::new(issue_list_url.path())))
         },
         _ => {
             // hard fail
-            Err("Not Supported")
+            Err(Error::new(ErrorKind::Unsupported, "Not Supported"))
         }
     };
 }
