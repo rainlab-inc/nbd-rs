6149bc1a727e8aa76e6bb54e6cd952cecb99f33f
Furkan Mustafa <furkan@s2n.io>
2021-11-22 22:06:52 +0900

    object: simpler config string

--- src/object/config.rs
+++ src/object/config.rs
@@ -1,26 +1,25 @@
 use std::io::{Error,ErrorKind};
-use url::{Url};
 
 use crate::object::ObjectStorage;
 use crate::object::FileBackend;
 use crate::object::S3Backend;
 
 pub fn object_storage_with_config(config: String) -> Result<Box<dyn ObjectStorage>, Error> {
-    let parsed_url = Url::parse(&config)
-        .expect("Failed to parse config (URL)");
+    // config sample; "file:/path/to/folder/"
+    // config sample; "s3:http://localhost:9000/test"
 
-    log::info!("object storage: {:?}", &parsed_url);
+    let mut split: Vec<&str> = config.split(":").collect();
+    let driver_name = split.remove(0);
+    let driver_config = split.join(":");
 
-    return match parsed_url.scheme() {
+    log::info!("object storage: {:?}({:?})", &driver_name, &driver_config);
+
+    return match driver_name {
         "file" => {
-            // Expecting a folder path
-            let mut path_from_url = parsed_url.as_str().splitn(2, "///");
-            path_from_url.next().unwrap(); // 'file:'
-            let path = path_from_url.next().unwrap_or("./");
-            Ok(Box::new(FileBackend::new(String::from(path))))
+            Ok(Box::new(FileBackend::new(driver_config)))
         },
-        "s3+http" | "s3+https" => {
-            Ok(Box::new(S3Backend::new(config.strip_prefix("s3+").unwrap().to_string())))
+        "s3" => {
+            Ok(Box::new(S3Backend::new(driver_config)))
         },
         _ => {
             // hard fail
