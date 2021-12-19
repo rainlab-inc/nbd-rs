22073aba63057dbcddd97cee4ea69496f82ee34f
Furkan Mustafa <furkan@s2n.io>
2021-12-19 13:08:32 +0900

    block: use block aligned iterator for `fill`
    to avoid allocating the whole range in memory at once, the range
    is chunked into 4MB blocks, aligned at 4MB offsets

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -11,7 +11,7 @@ pub use self::raw::RawBlock;
 mod sharded;
 pub use self::sharded::ShardedBlock;
 
-use crate::util::Propagation;
+use crate::util::{Propagation, AlignedBlockIter};
 
 pub trait BlockStorage {
     fn init(&mut self);
@@ -24,12 +24,18 @@ pub trait BlockStorage {
 
     // `fill` has a default implementation
     fn fill(&mut self, offset: u64, length: usize, fillbyte: u8) -> Result<Propagation, Error> {
-        // TODO: Don't allocate too big memory at once
-        //       Split this into 4MB chunks if bigger than 4M
-        //       And do it in an aligned fashion.
-        // if length > 4 * 1024 * 1024 { ... }
-        let filldata: Vec<u8> = vec![fillbyte; length as usize];
-        self.write(offset, length, &filldata)
+        // Don't allocate too big memory at once
+        // Split this into 4MB chunks if bigger than 4M
+        // And do it in an aligned fashion.
+        let mut overall_propagation : Propagation = Propagation::Guaranteed;
+        for r in (AlignedBlockIter{ from: offset as usize, to: offset as usize+length, blksize: 4*1024*1024 }) {
+            let filldata: Vec<u8> = vec![fillbyte; r.end - r.start];
+            let propagated = self.write(r.start as u64, r.end - r.start, &filldata)?;
+            if (propagated as u8) < (overall_propagation as u8) {
+                overall_propagation = propagated;
+            }
+        }
+        Ok(overall_propagation)
     }
 
     // default sub-optimal implementation for `trim`
