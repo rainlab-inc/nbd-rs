d5954a66205de9b7dca055e2437a4a89bd49a88b
Tan Aytekin <tanaytekin@riseup.net>
2022-04-12 14:44:15 +0300

    better test setup for shard distribution

--- src/block/shard_distribution.rs
+++ src/block/shard_distribution.rs
@@ -1,51 +1,61 @@
-pub fn node_idx_for_shard(shard_idx: usize, replicas: usize, nodes: usize) -> usize {
-    todo!()
+pub fn node_idx_for_shard(shard_idx: usize, replica_idx: u8, replicas: u8, nodes: u8) -> usize {
+    todo!();
+}
+
+#[derive(PartialEq)]
+pub struct ReplicaIdentity {
+    shard_idx: usize,
+    replica_idx: u8,
+}
+
+impl ReplicaIdentity {
+    fn new(shard_idx: usize, replica_idx: u8) -> ReplicaIdentity {
+        ReplicaIdentity {
+            shard_idx,
+            replica_idx,
+        }
+    }
+}
+
+pub struct DistributionSetup {
+    n_nodes: u8,
+    n_replicas: u8,
+    n_shards: usize,
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::block::shard_distribution::node_idx_for_shard;
-    #[test]
-    fn node_1_copy_1() {
-        for shard_idx in 0..50 {
-            let node_idx = node_idx_for_shard(shard_idx, 1, 1);
-            assert_eq!(node_idx, 0);
+    use super::*;
+
+    const N_SHARDS: usize = 50;
+
+    pub struct DistributionResult {
+        nodes: Vec<Vec<ReplicaIdentity>>,
+    }
+
+    impl DistributionResult {
+        fn new() -> DistributionResult {
+            let nodes: Vec<Vec<ReplicaIdentity>> = Vec::new();
+            DistributionResult { nodes }
         }
     }
 
-    #[test]
-    fn node_2_copy_1() {
-        let mut alter_flag = true;
-        for shard_idx in 0..50 {
-            let node_idx = node_idx_for_shard(shard_idx, 1, 2);
-            if alter_flag {
-                assert_eq!(shard_idx, 0);
-            } else {
-                assert_eq!(shard_idx, 1);
-            }
-            alter_flag = !alter_flag;
+    fn simulate_distribution(setup: DistributionSetup) -> DistributionResult {
+        let mut res = DistributionResult::new();
+        for node_idx in 0..setup.n_nodes {
+            let replicas_in_node: Vec<ReplicaIdentity> = Vec::new();
+            res.nodes.push(replicas_in_node);
         }
-    }
 
-    #[test]
-    fn node_2_copy_2() {
-        let mut alter_flag = true;
-        let mut alter_count = 0;
-
-        for shard_idx in 0..50 {
-            let node_idx = node_idx_for_shard(shard_idx, 2, 2);
-            if alter_flag {
-                assert_eq!(shard_idx, 0);
-            } else {
-                assert_eq!(shard_idx, 1);
-            }
-            alter_count = alter_count + 1;
-            if alter_count == 2 {
-                alter_flag = !alter_flag;
-                alter_count = 0;
+        for shard_idx in 0..setup.n_shards {
+            for replica_idx in 0..setup.n_replicas {
+                let node_idx =
+                    node_idx_for_shard(shard_idx, replica_idx, setup.n_replicas, setup.n_nodes);
+                let replica_iden = ReplicaIdentity::new(shard_idx, replica_idx);
+                res.nodes[node_idx].push(replica_iden);
             }
         }
+        res
     }
-
 }
