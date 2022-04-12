3a8d6fb3ad2059c11537d96b3efb61489b62848a
Tan Aytekin <tanaytekin@riseup.net>
2022-04-12 11:35:02 +0300

    node 2 copy 2 test

--- src/block/shard_distribution.rs
+++ src/block/shard_distribution.rs
@@ -28,5 +28,24 @@ mod tests {
         }
     }
 
+    #[test]
+    fn node_2_copy_2() {
+        let mut alter_flag = true;
+        let mut alter_count = 0;
+
+        for shard_idx in 0..50 {
+            let node_idx = node_idx_for_shard(shard_idx, 1, 1);
+            if alter_flag {
+                assert_eq!(shard_idx, 0);
+            } else {
+                assert_eq!(shard_idx, 1);
+            }
+            alter_count = alter_count + 1;
+            if alter_count == 2 {
+                alter_flag = !alter_flag;
+                alter_count = 0;
+            }
+        }
+    }
 
 }
