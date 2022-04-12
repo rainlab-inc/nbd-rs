0619bc345c605e8e52d594446e67f02f47cd5c49
Tan Aytekin <tanaytekin@riseup.net>
2022-04-12 11:32:51 +0300

    node 1 copy 1 test

--- src/block/shard_distribution.rs
+++ src/block/shard_distribution.rs
@@ -4,4 +4,14 @@ pub fn node_idx_for_shard(shard_idx: usize, replicas: usize, nodes: usize) -> us
 
 #[cfg(test)]
 mod tests {
+
+    use crate::block::shard_distribution::node_idx_for_shard;
+    #[test]
+    fn node_1_copy_1() {
+        for shard_idx in 0..50 {
+            let node_idx = node_idx_for_shard(shard_idx, 1, 1);
+            assert_eq!(node_idx, 0);
+        }
+    }
+
 }
