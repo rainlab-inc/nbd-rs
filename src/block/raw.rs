830c93865b4e030d43875fa9605d02d409830d34
Tan Aytekin <tanaytekin@riseup.net>
2022-06-23 15:41:09 +0300

    Add init_volume_from_remote() for raw

--- src/block/raw.rs
+++ src/block/raw.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Error}
+    io::{Error, ErrorKind}
 };
 use url::{Url};
 
@@ -16,6 +16,8 @@ pub struct RawBlock {
     name: String,
     volume_size: u64,
     object_storage: Box<dyn ObjectStorage>,
+    volume_initialized: bool,
+    config: BlockStorageConfig,
 }
 
 impl RawBlock {
@@ -33,43 +35,9 @@ impl RawBlock {
             name: String::from(filename),
             volume_size: 0_u64,
             object_storage: object_storage_with_config(String::from(new_config)).unwrap(),
+            volume_initialized: false,
+            config: config.clone(),
         };
-
-        if config.export_name.is_none() && config.export_size.is_some() {
-            // Initialize volume
-            let volume_size = config.export_size.unwrap() as u64;
-            log::info!("Volume size: {}", volume_size);
-            selfref.volume_size = volume_size;
-
-            /* Check initialized */
-            let size = selfref.object_storage.read("size".to_string());
-            if size.is_ok() {
-                /* Already initialized */
-                let size = String::from_utf8(selfref.object_storage.read("size".to_string()).unwrap()).unwrap();
-                let size: u64 = size.parse().unwrap();
-                if size == volume_size {
-                    log::warn!("Block storage is already initialized with the same size: {}", size);
-                } else {
-                    if !config.export_force {
-                        log::error!("Block storage is already initialized and the size is configured to be {}, add --force to override current configuration", size);
-                        panic!();
-                    } else {
-                        log::warn!("Block storage is already initialized with size: {}", size);
-                    }
-                }
-            }
-            let size_str = volume_size.to_string();
-            selfref.object_storage.write(String::from("size"), &size_str.as_bytes());
-            log::info!("Volume size is written.");
-
-        } else if config.export_name.is_some() && config.export_size.is_none() {
-            let size = String::from_utf8(selfref.object_storage.read("size".to_string()).unwrap()).unwrap();
-            let size: u64 = size.parse().unwrap();
-            log::info!("Volume size of the block stoage is {}", size);
-            selfref.volume_size = size;
-        } else {
-            panic!("Unreachable");
-        }
 
         selfref.init();
         selfref
@@ -82,6 +50,54 @@ impl BlockStorage for RawBlock {
             .start_operations_on_object(self.name.clone()).unwrap();
 
         self.volume_size = self.object_storage.get_size(self.name.clone()).unwrap_or(0);
+    }
+
+    fn init_volume(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        if self.volume_initialized {
+            return Err(Error::new(ErrorKind::Other, format!("Volume is already initialized.")).into());
+        }
+        
+        // Initialize volume
+        let volume_size = self.config.export_size.unwrap() as u64;
+        log::info!("Volume size: {}", volume_size);
+        self.volume_size = volume_size;
+            
+        /* Check initialized */
+        let size = self.object_storage.read("size".to_string());
+        if size.is_ok() {
+            /* Already initialized */
+            let size = String::from_utf8(self.object_storage.read("size".to_string()).unwrap()).unwrap();
+            let size: u64 = size.parse().unwrap();
+            if size == volume_size {
+                log::warn!("Block storage is already initialized with the same size: {}", size);
+            } else {
+                if !self.config.export_force {
+                    return Err(Error::new(ErrorKind::Other, format!("Block storage is already initialized and the size is configured to be {}, add --force to override current configuration", size )).into());
+                } else {
+                    log::warn!("Block storage is already initialized with size: {}", size);
+                }
+            }
+        }
+            
+        let size_str = volume_size.to_string();
+        self.object_storage.write(String::from("size"), &size_str.as_bytes());
+        log::info!("Volume size is written.");
+        
+        self.volume_initialized = true;
+        Ok(())
+    }
+    
+    fn init_volume_from_remote(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        if self.config.export_name.is_some() && self.config.export_size.is_none() {
+            let size = String::from_utf8(self.object_storage.read("size".to_string()).unwrap()).unwrap();
+            let size: u64 = size.parse().unwrap();
+            log::info!("Volume size of the block stoage is {}", size);
+            self.volume_size = size;
+            self.volume_initialized = true;
+            Ok(())
+        } else {
+            return Err(Error::new(ErrorKind::Other, format!("init_volume_from_remote() is failed.")).into());
+        }
     }
 
     fn get_name(&self) -> String {
